492c2fb138f40a7f0a72eebbe257b5843fd93717
dream@fedora <25shubhamyadav25@gmail.com>
2026-07-10 14:14:06 +0530

    Implement shell loop

--- shell.c
+++ shell.c
@@ -4,6 +4,25 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("fsh$> ");
+        line = fsh_read_line();
+        args = fsh_split_line(line);
+        status = fsh_execute(args);
+
+        free(line);
+        free(args);
+        
+    }
+}
+
+
 int main(int argc, char **argv){
     //Load config files 
 

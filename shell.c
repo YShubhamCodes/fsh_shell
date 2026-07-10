c51563b3f00529b1d6d04a01a7c27934ed981ed7
dream@fedora <25shubhamyadav25@gmail.com>
2026-07-10 17:00:05 +0530

    Implement split_line function (Parsing the shell)

--- shell.c
+++ shell.c
@@ -55,3 +55,59 @@ char* fsh_read_line(void){
     return line;
 }
 
+void fsh_split_line(char* line, Pipeline *pipeline){
+    pipeline->command_count = 0;
+
+    char *pipe_saveptr;
+    // Split the entire line by the pipe character '|'
+    char *cmd_segment = strtok_r(line, "|", &pipe_saveptr);
+
+    while(cmd_segment != NULL && pipeline->command_count < MAX_COMMANDS) {
+        // Get a pointer to the current struct block we are building
+        Command *current_cmd = &pipeline->commands[pipeline->command_count];
+
+        // Initialize struct fields
+        current_cmd->input_file = NULL;
+        current_cmd->output_file = NULL;
+        int arg_idx = 0;
+
+        char *arg_saveptr;
+        // Split the current segment by spaces to isolate words
+        char *token = strtok_r(cmd_segment, " \t\r\n\a", &arg_saveptr);
+
+        while(token != NULL){
+            if(strcmp(token, "<") == 0) {
+                // Next token is the input file
+                token = strtok_r(NULL, " \t\r\n\a", &arg_saveptr);
+                if(token != NULL){
+                    current_cmd->input_file = token;
+                }
+            }
+            else if(strcmp(token, ">") == 0){
+                // Next token is the output file
+                token = strtok_r(NULL, " \t\r\n\a", &arg_saveptr);
+                if(token != NULL){
+                    current_cmd->output_file = token;
+                }
+            }
+            else{
+                // standard command argument
+                if(arg_idx < MAX_ARGS - 1){
+                    current_cmd->argv[arg_idx++] = token;
+                }
+            }
+            // Move to the next token in this segment
+            token = strtok_r(NULL, " \t\r\n\a", &arg_saveptr);
+        }
+        // Explicitly NULL terminate the argv array after the prompt/segment
+        current_cmd->argv[arg_idx] = NULL;
+
+        // increament command count
+        if(arg_idx > 0){
+            pipeline->command_count++;
+        }
+
+        // Move to the next pipe segment
+        cmd_segment = strtok_r(NULL, "|", &pipe_saveptr);
+    }
+}
